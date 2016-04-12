06952df9f8900a3885e5409bf37d993ab5dbc92b
peter huang <Peter.Y.Huang@emc.com>
2016-04-12 09:28:46 +0800

    use form::file class

--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -3,10 +3,12 @@
 <div class="ui container" style="padding-top: 5em">
 	<div class="row">
 		<h1>Upload picture</h1>
-
+	/*
+	 * https://laravelcollective.com/docs/5.2/html#
+	 * /
 	{!! Form::open(array('route'=>'photo.store', 'class'=>'form', 'files'=>true))!!}
 	{!! Form::label('picture')!!}
-<input name="photo" id="photo" type="file" />	
+	{!! Form::file('photo') !!}
 	{!! Form::submit('ok') !!}
 	{!! Form::close() !!}
 	</div>

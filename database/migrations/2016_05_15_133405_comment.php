536a2fc85ed51e94a0492bc02cc392cecf488bd5
huang yi <huangered@gmail.com>
2016-05-15 21:57:03 +0800

    add photo delete feature

---
+++ database/migrations/2016_05_15_133405_comment.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Comment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_image_id_foreign');
+            $table->dropForeign('comments_user_id_foreign');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
